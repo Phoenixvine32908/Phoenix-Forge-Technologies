573c3710d982674256d27dc3768d27047eb3ca12
Connor Smith <phoenixvine32908@gmail.com>
2025-04-02 23:08:50 -0400

    more fixes

--- kubejs/server_scripts/gregtech/melferious_matrix.js
+++ kubejs/server_scripts/gregtech/melferious_matrix.js
@@ -170,7 +170,7 @@ makeMatrixRecipes(
     addOreBeeRecipe('vanadium_magnetite');
 //////////////////// Wanna Section ////////////////
 function addWannaBeeRecipe(beeName, mobEntityType) {
-    const recipeBuilder = phoenixvine.recipes.gtceu.melferious_matrix(`kubejs:gtceu/melferious_matrix/wanna_${beeName.toLowerCase().replace(':', '_')}`)
+    const recipeBuilder = phoenixvine.recipes.gtceu.melferious_matrix(`kubejs:gtceu/melferious_matrix/wanna_${beeName.toLowerCase().replace(' ', '_')}`)
         .circuit(1)
         .EUt(1048)
         .duration(300)
@@ -180,21 +180,92 @@ function addWannaBeeRecipe(beeName, mobEntityType) {
     return recipeBuilder; // Return the recipe builder to chain output definitions
 }
 
+const piglinRecipe = addWannaBeeRecipe('Piglin', 'minecraft:piglin');
+piglinRecipe.chancedOutput('minecraft:gold_ingot', 6000, 1); // 60%
+piglinRecipe.chancedOutput('minecraft:rotten_flesh', 4000, 1); // 40%
+piglinRecipe.chancedOutput('minecraft:porkchop', 3000, 1); // 30%
+
+const hoglinRecipe = addWannaBeeRecipe('Hoglin', 'minecraft:hoglin');
+hoglinRecipe.chancedOutput('minecraft:porkchop', 7000, 2); // 70%
+hoglinRecipe.chancedOutput('minecraft:leather', 5000, 1); // 50%
+
+const zombifiedPiglinRecipe = addWannaBeeRecipe('Zombified Piglin', 'minecraft:zombified_piglin');
+zombifiedPiglinRecipe.chancedOutput('minecraft:gold_ingot', 5000, 1); // 50%
+zombifiedPiglinRecipe.chancedOutput('minecraft:rotten_flesh', 6000, 2); // 60%
+
+const zombifiedHoglinRecipe = addWannaBeeRecipe('Zombified Hoglin', 'minecraft:zombified_hoglin');
+zombifiedHoglinRecipe.chancedOutput('minecraft:rotten_flesh', 7500, 2); // 75%
+zombifiedHoglinRecipe.chancedOutput('minecraft:leather', 4000, 1); // 40%
+
+const piglinBruteRecipe = addWannaBeeRecipe('Piglin Brute', 'minecraft:piglin_brute');
+piglinBruteRecipe.chancedOutput('minecraft:gold_ingot', 8000, 2); // 80%
+piglinBruteRecipe.chancedOutput('minecraft:rotten_flesh', 2000, 1); // 20%
+piglinBruteRecipe.chancedOutput('minecraft:porkchop', 4000, 1); // 40%
+
+const shulkerRecipe = addWannaBeeRecipe('Shulker', 'minecraft:shulker');
+shulkerRecipe.chancedOutput('minecraft:shulker_shell', 6500, 1); // 65%
+
+const phantomRecipe = addWannaBeeRecipe('Phantom', 'minecraft:phantom');
+phantomRecipe.chancedOutput('minecraft:phantom_membrane', 7000, 1); // 70%
+
+const blazeRecipe = addWannaBeeRecipe('Blaze', 'minecraft:blaze');
+blazeRecipe.chancedOutput('minecraft:blaze_rod', 6000, 1); // 60%
+
+const elderGuardianRecipe = addWannaBeeRecipe('Elder Guardian', 'minecraft:elder_guardian');
+elderGuardianRecipe.chancedOutput('minecraft:prismarine_shard', 5000, 3); // 50%
+elderGuardianRecipe.chancedOutput('minecraft:prismarine_crystals', 4000, 2); // 40%
+elderGuardianRecipe.chancedOutput('minecraft:sponge', 1000, 1); // 10%
+
+const evokerRecipe = addWannaBeeRecipe('Evoker', 'minecraft:evoker');
+evokerRecipe.chancedOutput('minecraft:totem_of_undying', 1500, 1); // 15%
+evokerRecipe.chancedOutput('minecraft:emerald', 5000, 2); // 50%
+
+const magmaCubeRecipe = addWannaBeeRecipe('Magma Cube', 'minecraft:magma_cube');
+magmaCubeRecipe.chancedOutput('minecraft:magma_cream', 7500, 2); // 75%
+
+const wardenRecipe = addWannaBeeRecipe('Warden', 'minecraft:warden');
+wardenRecipe.chancedOutput('minecraft:sculk_catalyst', 1000, 1); // 10%
+wardenRecipe.chancedOutput('minecraft:echo_shard', 3000, 2); // 30%
+
+const illagerRecipe = addWannaBeeRecipe('Illager', 'minecraft:pillager'); // Using pillager as a general illager
+illagerRecipe.chancedOutput('minecraft:crossbow', 4000, 1); // 40%
+
+const slimeRecipe = addWannaBeeRecipe('Slime', 'minecraft:slime');
+slimeRecipe.chancedOutput('minecraft:slime_ball', 8000, 3); // 80%
+
+const vindicatorRecipe = addWannaBeeRecipe('Vindicator', 'minecraft:vindicator');
+vindicatorRecipe.chancedOutput('minecraft:emerald', 5500, 1); // 55%
+vindicatorRecipe.chancedOutput('minecraft:iron_axe', 4000, 1); // 40%
+
+const witchRecipe = addWannaBeeRecipe('Witch', 'minecraft:witch');
+witchRecipe.chancedOutput('minecraft:glass_bottle', 5000, 1); // 50%
+witchRecipe.chancedOutput('minecraft:glowstone_dust', 3000, 1); // 30%
+witchRecipe.chancedOutput('minecraft:redstone', 3000, 1); // 30%
+witchRecipe.chancedOutput('minecraft:spider_eye', 3000, 1); // 30%
+witchRecipe.chancedOutput('minecraft:sugar', 3000, 1); // 30%
+witchRecipe.chancedOutput('minecraft:stick', 3000, 1); // 30%
+witchRecipe.chancedOutput('minecraft:gunpowder', 3000, 1); // 30%
+
+const witherSkeletonRecipe = addWannaBeeRecipe('Wither Skeleton', 'minecraft:wither_skeleton');
+witherSkeletonRecipe.chancedOutput('minecraft:coal', 6000, 1); // 60%
+witherSkeletonRecipe.chancedOutput('minecraft:bone', 5000, 1); // 50%
+witherSkeletonRecipe.chancedOutput('minecraft:wither_skeleton_skull', 500, 1); // 5%
+
 const creeperRecipe = addWannaBeeRecipe('Creeper', 'minecraft:creeper');
 creeperRecipe.chancedOutput('minecraft:gunpowder', 7500, 2); // 75%
 
 const zombieRecipe = addWannaBeeRecipe('Zombie', 'minecraft:zombie');
 zombieRecipe.chancedOutput('minecraft:rotten_flesh', 9000, 3); // 90%
-zombieRecipe.chancedOutput('minecraft:iron_ingot', 500, 1);   // 5%
-zombieRecipe.chancedOutput('minecraft:carrot', 500, 1);     // 5%
-zombieRecipe.chancedOutput('minecraft:potato', 500, 1);     // 5%
+zombieRecipe.chancedOutput('minecraft:iron_ingot', 500, 1);    // 5%
+zombieRecipe.chancedOutput('minecraft:carrot', 500, 1);      // 5%
+zombieRecipe.chancedOutput('minecraft:potato', 500, 1);      // 5%
 
 const skeletonRecipe = addWannaBeeRecipe('Skeleton', 'minecraft:skeleton');
-skeletonRecipe.chancedOutput('minecraft:bone', 8000, 2);     // 80%
-skeletonRecipe.chancedOutput('minecraft:arrow', 500, 1);   // 5%
+skeletonRecipe.chancedOutput('minecraft:bone', 8000, 2);      // 80%
+skeletonRecipe.chancedOutput('minecraft:arrow', 500, 1);     // 5%
 
 const spiderRecipe = addWannaBeeRecipe('Spider', 'minecraft:spider');
-spiderRecipe.chancedOutput('minecraft:string', 8500, 4);     // 85%
+spiderRecipe.chancedOutput('minecraft:string', 8500, 4);      // 85%
 spiderRecipe.chancedOutput('minecraft:spider_eye', 1000, 1); // 10%
 
 const endermanRecipe = addWannaBeeRecipe('Enderman', 'minecraft:enderman');
@@ -202,7 +273,7 @@ endermanRecipe.chancedOutput('minecraft:ender_pearl', 6000, 1); // 60%
 
 const ghastRecipe = addWannaBeeRecipe('Ghast', 'minecraft:ghast');
 ghastRecipe.chancedOutput('minecraft:ghast_tear', 7000, 1); // 70%
-ghastRecipe.chancedOutput('minecraft:gunpowder', 2000, 1);   // 20%
+ghastRecipe.chancedOutput('minecraft:gunpowder', 2000, 1);    // 20%
 
 const witherRecipe = addWannaBeeRecipe('Wither', 'minecraft:wither');
 witherRecipe.chancedOutput('minecraft:nether_star', 1000, 1); // 10%
@@ -212,11 +283,11 @@ enderDragonRecipe.chancedOutput('minecraft:dragon_breath', 5000, 5); // 50%
 
 const cowRecipe = addWannaBeeRecipe('Cow', 'minecraft:cow');
 cowRecipe.chancedOutput('minecraft:leather', 9500, 2); // 95%
-cowRecipe.chancedOutput('minecraft:beef', 9000, 2);   // 90%
+cowRecipe.chancedOutput('minecraft:beef', 9000, 2);    // 90%
 
 const rabbitRecipe = addWannaBeeRecipe('Rabbit', 'minecraft:rabbit');
-rabbitRecipe.chancedOutput('minecraft:rabbit_hide', 8000, 1);   // 80%
-rabbitRecipe.chancedOutput('minecraft:rabbit_foot', 1000, 1);   // 10%
+rabbitRecipe.chancedOutput('minecraft:rabbit_hide', 8000, 1);    // 80%
+rabbitRecipe.chancedOutput('minecraft:rabbit_foot', 1000, 1);    // 10%
 rabbitRecipe.chancedOutput('minecraft:cooked_rabbit', 7000, 1); // 70%
 
 const pigRecipe = addWannaBeeRecipe('Pig', 'minecraft:pig');
@@ -224,5 +295,5 @@ pigRecipe.chancedOutput('minecraft:porkchop', 9200, 3); // 92%
 
 const sheepRecipe = addWannaBeeRecipe('Sheep', 'minecraft:sheep');
 sheepRecipe.chancedOutput('minecraft:mutton', 9000, 2); // 90%
-sheepRecipe.chancedOutput('minecraft:wool', 8500, 1);   // 85%
+sheepRecipe.chancedOutput('minecraft:wool', 8500, 1);    // 85%
     });
