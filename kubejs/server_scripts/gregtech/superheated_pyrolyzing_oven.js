1fdc4bb2367ecb0e9c963a228c218914caf24b41
Connor Smith <phoenixvine32908@gmail.com>
2025-06-16 20:11:12 -0400

    hmm

--- kubejs/server_scripts/gregtech/superheated_pyrolyzing_oven.js
+++ kubejs/server_scripts/gregtech/superheated_pyrolyzing_oven.js
@@ -1,61 +1,107 @@
-/*ServerEvents.recipes(allthemods => {
+ServerEvents.recipes(allthemods => {
     allthemods.forEachRecipe({ type: 'gtceu:pyrolyse_oven' }, rawRecipe => {
+        // get variables from raw recipe
         let recipe = JSON.parse(rawRecipe.json)
         let inputItemsArray = recipe.inputs.item
         let inputFluidsArray = recipe.inputs.fluid
         let outputItemsArray = recipe.outputs.item
         let outputFluidsArray = recipe.outputs.fluid
-
         let duration = recipe.duration
+        // added power variable because I forgot to mention it before
+        let power = parseInt(recipe.tickInputs.eu[0].content)
+        let baseId = rawRecipe.getId() + '_base'
+        let boostedId = rawRecipe.getId() + '_boosted'
+
+    
+        // made the recipe builders into separate variables so we can split up recipe building across multiple lines
+        let baseRecipe = allthemods.recipes.gtceu.superheated_pyrolyzing_oven(baseId)
+        let boostedRecipe = allthemods.recipes.gtceu.superheated_pyrolyzing_oven(boostedId)
+
+        let multiplier = 1000
         let circuit = -1
         let notConsumable = []
-        let itemInputs = [itemInputs]
-        let fluidInputs = [fluidInputs]
-        let itemOutputs = [itemOutputs]
-        let outputFluids = [outputFluids]
-        
+        let itemInputs = []
+        let fluidInputs = []
+        let itemOutputs = []
+        let fluidOutputs = []
+
+           // take our JSON recipe input items and determine if they are circuits or items, consumed or not consumed
+        // then add them to our itemInputs array as Ingredient objects
         inputItemsArray.forEach(inputItem => {
+            // check if it is a circuit
             if (inputItem.content.type == 'gtceu:circuit') {
-                circuit = inputItem.content.configuration // just grab the circuit number
+                circuit = inputItem.content.configuration // if it is, just grab the circuit number
             } else if (inputItem.content.type == 'gtceu:sized') {
-                if (inputItem.chance == 0) {
-                    notConsumable.push(Ingredient.of(inputItem.content.ingredient, inputItem.content.count))
-                } else {
+                // check if the chance on consuming this input is 10000
+                if (inputItem.chance == 10000) {
                     itemInputs.push(Ingredient.of(inputItem.content.ingredient, inputItem.content.count))
+                } else { // otherwise log it
+                    console.log("Oops, chance was not 10000 for " + JSON.stringify(inputItem.content.ingredient))
                 }
+            } else { // we did not account for this type of ingredient...
+                console.log("Ingredient type " + inputItem.content.type + " not accounted for, please report this")
             }
         })
 
+        // take our JSON recipe input fluids and add them into an array of Fluid objects
+        // TODO: advanced - handle chanced fluid inputs
+        // TODO: are there any input fluids in this recipe? 
+        //       add an if loop to check
         inputFluidsArray.forEach(fluidInput => {
             fluidInputs.push(Fluid.of(fluidInput.content.value, fluidInput.content.amount))
         })
 
+        // take our JSON recipe output items and add them into an array of Ingredient objects
+        // TODO: advanced - handle chanced item outputs
         outputItemsArray.forEach(outputItem => {
             itemOutputs.push(Ingredient.of(outputItem.content.ingredient, outputItem.content.count))
         })
 
+        // take our JSON recipe output fluids and add them into an array of Fluid objects
+        // TODO: advanced - handle chanced fluid outputs
+        // TODO: are there any output fluids in this recipe? 
+        //       add an if loop to check
         outputFluidsArray.forEach(fluidOutput => {
-            outputFluids.push(Fluid.of(fluidOutput.content.ingredient, fluidOutput.content.amount))
+            fluidOutputs.push(Fluid.of(fluidOutput.content.ingredient, fluidOutput.content.amount))
         })
-        let base_id = rawRecipe.getId() + '_base'
-        allthemods.recipes.gtceu.superheated_pyrolyzing_oven(base_id)
-        .circuit(circuit)
-        .duration(duration)
-        .itemInputs(itemInputs)
-        .itemOutputs(itemOutputs)
-        .itemOutputs(itemOutputs)
-        .outputFluids(outputFluids)
-        .EUt(power)
-        let multiplier = 1000
-        let boosted_id = rawRecipe.getId() + '_boosted'
-        allthemods.recipes.gtceu.superheated_pyrolyzing_oven(boosted_id)
-        .circuit(circuit)
-        .itemInputs(Ingredient.of(itemInputs.getCount() * multiplier))
-        .itemOutputs(Ingredient.of(itemOutputs.getCount() * multiplier))
-        .inputFluids(Fluid.of(inputFluids.getCount() * multiplier))
-        .outputFluids(Fluid.of(outputFluids.getCount() * multiplier))
-        .duration(duration)
-        .EUt(power)
+
+        baseRecipe.duration(duration)
+        baseRecipe.EUt(power)
+
+        // TODO: was there a circuit number for this recipe?
+        //       add an if statement to conditionally call the .circuit() method if there was a circuit number
+        if (circuit != -1) {
+            baseRecipe.circuit(circuit)
+            boostedRecipe.circuit(circuit)
+        }
+        // TODO: are there any Ingredient objects in our itemInputs array?
+        //       if present, add them to the baseRecipe
+
+        // TODO: are there any Fluid objects in our fluidInputs array?
+        //       if present, add them to the baseRecipe
+        
+        // TODO: are there any Ingredient objects in our itemOutputs array?
+        //       if present, add them to the baseRecipe
+
+        // TODO: are there any Fluid objects in our fluidOutputs array?
+        //       if present, add them to the baseRecipe
+ allthemods.recipes.gtceu.superheated_pyrolyzing_oven(baseId)
+            .circuit(circuit)
+            .duration(duration)
+            .itemInputs(itemInputs)
+            .inputFluids(fluidInputs)
+            .itemOutputs(itemOutputs)
+            .outputFluids(fluidOutputs)
+            .EUt(power)
+
+        allthemods.recipes.gtceu.superheated_pyrolyzing_oven(boostedId)
+            .circuit(circuit)
+            .itemInputs(Ingredient.of(itemInputs, itemInputs.getCount() * multiplier))
+            .itemOutputs(Ingredient.of(itemOutputs, itemOutputs.getCount() * multiplier))
+            .inputFluids(Fluid.of(fluidInputs, fluidInputs.getCount() * multiplier))
+            .outputFluids(Fluid.of(fluidOutputs, fluidOutputs.getCount() * multiplier))
+            .duration(duration)
+            .EUt(power)
+       
     })
 });
-*/
