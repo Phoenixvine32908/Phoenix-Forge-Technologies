b29d210d0430f972fbf4b2661fcb3303ea14d873
Connor Smith <phoenixvine32908@gmail.com>
2026-02-02 01:29:17 -0500

    d

--- kubejs/startup_scripts/deprecations.js
+++ kubejs/startup_scripts/deprecations.js
@@ -14,10 +14,13 @@
 // deprecateFluid("gtceu:naq_fuel_t1", "gtceu:crude_naquadah_fuel", "T1 Naq Fuel")
 
 // Fission Blocks
-deprecateBlock("kubejs:fissile_reaction_safe_casing", "phoenixcore:fissile_reaction_safe_casing", "Fissile Reaction Safe Casing")
-deprecateBlock("kubejs:fissile_heat_safe_casing", "phoenixcore:fissile_heat_safe_casing", "Fissile Heat Safe Casing")
-deprecateBlock("kubejs:fissile_safe_gearbox", "phoenixcore:fissile_safe_gearbox_casing", "Fissile Safe Gearbox Casing")
-deprecateBlock("kubejs:fission_cooling_casing", "phoenixcore:basic_cooler", "Basic Cooler")
-deprecateBlock("kubejs:base_fission_casing", "phoenixcore:graphite_moderator", "Graphite Moderator")
+deprecateBlock("phoenixcore:fissile_reaction_safe_casing", "phoenix_fission:fissile_reaction_safe_casing", "Fissile Reaction Safe Casing")
+deprecateBlock("phoenixcore:fissile_heat_safe_casing", "phoenix_fission:fissile_heat_safe_casing", "Fissile Heat Safe Casing")
+deprecateBlock("phoenixcore:fissile_safe_gearbox_casing", "phoenix_fission:fissile_safe_gearbox_casing", "Fissile Safe Gearbox Casing")
+deprecateBlock("phoenixcore:basic_cooler", "phoenix_fission:basic_cooler", "Basic Cooler")
+deprecateBlock("phoenixcore:graphite_moderator", "phoenix_fission:graphite_moderator", "Graphite Moderator")
+deprecateBlock("phoenixcore:pressurized_fission_reactor", "phoenix_fission:pressurized_fission_reactor", "Pressurized Fission Reactor")
+deprecateBlock("phoenixcore:advanced_pressurized_fission_reactor", "phoenix_fission:advanced_pressurized_fission_reactor", "Advanced Pressurized Fission Reactor")
+deprecateBlock("phoenixcore:high_performance_breeder_reactor", "phoenix_fission:high_performance_breeder_reactor", "High Performance Breeder Reactor")
 
 
